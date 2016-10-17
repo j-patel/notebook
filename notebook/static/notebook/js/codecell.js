1dd46b0ad653365e5afc3821f8e15f65da6c284b
Jay Patel <jay.20991@gmail.com>
2016-10-17 16:33:07 -0400

    Modified codecell.js
    This file contains all the modifications I have done till now. First commit.

--- notebook/static/notebook/js/codecell.js
+++ notebook/static/notebook/js/codecell.js
@@ -11,6 +11,7 @@
 
 define([
     'base/js/namespace',
+    'jquery',
     'base/js/utils',
     'base/js/keyboard',
     'services/config',
@@ -22,6 +23,7 @@ define([
     'codemirror/mode/python/python',
     'notebook/js/codemirror-ipython'
 ], function(IPython,
+    $,
     utils,
     keyboard,
     configmod,
@@ -96,7 +98,7 @@ define([
         this.tooltip = options.tooltip;
         this.config = options.config;
         this.class_config = new configmod.ConfigWithDefaults(this.config,
-                                        CodeCell.options_default, 'CodeCell');
+                                        CodeCell.config_defaults, 'CodeCell');
 
         // create all attributed in constructor function
         // even if null for V8 VM optimisation
@@ -108,7 +110,7 @@ define([
         this.completer = null;
 
         Cell.apply(this,[{
-            config: options.config, 
+            config: $.extend({}, CodeCell.options_default), 
             keyboard_manager: options.keyboard_manager, 
             events: this.events}]);
 
@@ -123,7 +125,11 @@ define([
     CodeCell.options_default = {
         cm_config : {
             extraKeys: {
+                "Tab" :  "indentMore",
+                "Shift-Tab" : "indentLess",
                 "Backspace" : "delSpaceToPrevTabStop",
+                "Cmd-/" : "toggleComment",
+                "Ctrl-/" : "toggleComment"
             },
             mode: 'text',
             theme: 'ipython',
@@ -140,6 +146,8 @@ define([
             'magic_text/x-cython' :{'reg':['^%%cython']},
         },
     };
+
+    CodeCell.config_defaults = CodeCell.options_default;
 
     CodeCell.msg_cells = {};
 
@@ -169,8 +177,6 @@ define([
             if (that.keyboard_manager) {
                 that.keyboard_manager.enable();
             }
-
-            that.code_mirror.setOption('readOnly', !that.is_editable());
         });
         this.code_mirror.on('keydown', $.proxy(this.handle_keyevent,this));
         $(this.code_mirror.getInputField()).attr("spellcheck", "false");
@@ -181,12 +187,10 @@ define([
         cell.append(input).append(output);
         this.element = cell;
         this.output_area = new outputarea.OutputArea({
-            config: this.config,
-            selector: output,
-            prompt_area: true,
-            events: this.events,
-            keyboard_manager: this.keyboard_manager,
-        });
+            selector: output, 
+            prompt_area: true, 
+            events: this.events, 
+            keyboard_manager: this.keyboard_manager});
         this.completer = new completer.Completer(this, this.events);
     };
 
@@ -349,11 +353,9 @@ define([
             },
             iopub : {
                 output : function() { 
-                    that.events.trigger('set_dirty.Notebook', {value: true});
                     that.output_area.handle_output.apply(that.output_area, arguments);
                 }, 
                 clear_output : function() { 
-                    that.events.trigger('set_dirty.Notebook', {value: true});
                     that.output_area.handle_clear_output.apply(that.output_area, arguments);
                 }, 
             },
@@ -478,7 +480,6 @@ define([
         var prompt_html = CodeCell.input_prompt_function(this.input_prompt_number, nline);
         // This HTML call is okay because the user contents are escaped.
         this.element.find('div.input_prompt').html(prompt_html);
-        this.events.trigger('set_dirty.Notebook', {value: true});
     };
 
 
@@ -515,22 +516,41 @@ define([
                 this.code_mirror.clearHistory();
                 this.auto_highlight();
             }
-            this.set_input_prompt(data.execution_count);
+            alert("FromJSON2:"+data.cell_uuid);
+            //this.set_input_prompt(data.execution_count);
+//Edit
+            this.set_input_prompt(data.cell_uuid);
             this.output_area.trusted = data.metadata.trusted || false;
             this.output_area.fromJSON(data.outputs, data.metadata);
         }
     };
 
+    //http://stackoverflow.com/a/105074/2609272
+    //RFC 4122 uuid.v4() is recommended over this method
+    function guid() {
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+    }
+    
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
 
     CodeCell.prototype.toJSON = function () {
         var data = Cell.prototype.toJSON.apply(this);
         data.source = this.get_text();
         // is finite protect against undefined and '*' value
+
         if (isFinite(this.input_prompt_number)) {
             data.execution_count = this.input_prompt_number;
         } else {
-            data.execution_count = null;
+            data.execution_count = null; 
+             
         }
+//Edit - Jay
+        data.cell_uuid = guid();
         var outputs = this.output_area.toJSON();
         data.outputs = outputs;
         data.metadata.trusted = this.output_area.trusted;
